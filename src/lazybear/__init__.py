81519a7355b3f7e89b5b6edfca224f35244d5a28
dcronkite <dcronkite@gmail.com>
2026-05-26 15:02:16 -0700

    Added version info accessibility

--- src/lazybear/__init__.py
+++ src/lazybear/__init__.py
@@ -1,5 +1,6 @@
 from lazybear.expressions import col, lit
 from lazybear.sql import scan_df, scan_sql_query, scan_table
+from lazybear._version import __version__
 
 __all__ = [
     'scan_table', 'col', 'scan_sql_query', 'scan_df', 'lit',
